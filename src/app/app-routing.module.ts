3f75f6ce610a2b52ba8027ec86ea52744d932f37
niklashedstrom <karlniklashedstrom@outlook.com>
2019-03-07 12:48:14 +0100

    all routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BoendeComponent } from './boende/boende.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NollningComponent } from './nollning/nollning.component';
+import { PluggComponent } from './plugg/plugg.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'boende', component: BoendeComponent }
+  { path: 'boende', component: BoendeComponent },
+  { path: 'plugg', component: PluggComponent },
+  { path: 'nollning', component: BoendeComponent }
+  
 
 ];
 
