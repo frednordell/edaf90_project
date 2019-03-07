0dd3a7f964a8c2b0afab5e64edd77d28622d2bdc
niklashedstrom <karlniklashedstrom@outlook.com>
2019-03-07 12:42:58 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoendeComponent } from './boende/boende.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'boende', component: BoendeComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
